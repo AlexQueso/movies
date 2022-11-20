05e319310f585062fcdadb30f1f9917de2040a0e
Alex Quesada <a.quemen@gmail.com>
2022-11-20 13:09:07 +0100

    frequente renter points calculation is Movie class responsability

--- src/main/java/borrero/quesada/movies/Rental.java
+++ src/main/java/borrero/quesada/movies/Rental.java
@@ -23,9 +23,6 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints(){
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) {
-            return 2;
-        }
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
