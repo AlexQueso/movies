29fa5d34dd2427b536d5de3160aba83e26468dbc
Alex Quesada <a.quemen@gmail.com>
2022-11-20 13:05:58 +0100

    refactor no need to use Movie nor this inside Movie Class

--- src/main/java/borrero/quesada/movies/Movie.java
+++ src/main/java/borrero/quesada/movies/Movie.java
@@ -28,17 +28,17 @@ public class Movie {
 
     public double getCharge(int daysRented){
         double result = 0;
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
+        switch (priceCode) {
+            case REGULAR:
                 result += 2;
                 if (daysRented > 2) {
                     result += (daysRented - 2) * 1.5;
                 }
                 break;
-            case Movie.NEW_RELEASE:
+            case NEW_RELEASE:
                 result += 3;
                 break;
-            case Movie.CHILDREN:
+            case CHILDREN:
                 result += 1.5;
                 if (daysRented > 3) {
                     result += (daysRented - 1) * 1.5;
